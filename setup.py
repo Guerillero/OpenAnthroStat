0036e1d8c1f141a22527f8da7057f075f9164054
Tom Fish <guerillero.wikipedia@gmail.com>
2015-05-20 23:43:53 -0400

    version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='OpenAnthroStat',
-    version='0.1.1',
+    version='0.2.0',
     packages=[''],
     url='https://github.com/Guerillero/OpenAnthroStat',
     license='MIT',
